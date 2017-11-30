135d344b3afd673e2f1b76e3cd9e24aa06b3e132
Kenneth Blix <kennethblix@gmail.com>
2017-11-30 20:20:48 +0100

    Adding value to textbox, using event sender on button.

--- SimpleCalculator/SimpleCalculator/Form1.cs
+++ SimpleCalculator/SimpleCalculator/Form1.cs
@@ -12,9 +12,22 @@ namespace SimpleCalculator
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (TextBoxResult.Text == "0")
+            {
+                TextBoxResult.Clear();
+            }
+            Button button = (Button)sender;
+            TextBoxResult.Text = TextBoxResult.Text + button.Text;
+        }
+
+
     }
 }
